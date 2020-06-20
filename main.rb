5530de59e4cc9f584f169b6e3d9bbb784f466417
Sam Eff <HeySamEff@gmail.com>
2020-06-20 11:48:56 -0700

    created skeleton for classes

---
+++ main.rb
@@ -0,0 +1,65 @@
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize
+    @value = nil
+    @next_node = nil
+  end
+
+end
+
+class LinkedList
+
+  def initialize
+    @list = []
+  end
+
+  def append(value)
+
+  end
+
+  def prepend(value)
+
+  end
+
+  def size
+
+  end
+
+  def head
+
+  end
+
+  def tail
+
+  end
+
+  def at(index)
+
+  end
+
+  def pop
+
+  end
+
+  def contains?(valule)
+
+  end
+
+  def find(value)
+
+  end
+
+  def to_s
+
+  end
+
+  def insert_at(value, index)
+
+  end
+
+  def remove_at(index)
+
+  end
+
+end
